53629ea4bf4b3e10401ec19eaa600e2ac09347ec
ramazan_aisaev <aisaev-68@yandex.ru>
2023-10-07 15:34:25 +0300

    fix

--- catalog/views.py
+++ catalog/views.py
@@ -2,12 +2,9 @@ import logging
 from datetime import datetime
 from django.conf import settings
 from django.db.models import Q, Avg, Count, ObjectDoesNotExist
-from django.core.paginator import Paginator
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
-from django.db.models import F, FloatField
-from django.db.models.functions import Cast
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from rest_framework.views import APIView
