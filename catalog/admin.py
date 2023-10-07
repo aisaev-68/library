11ff6e38e947b2bbb0476a5b62a249fea4d02f54
ramazan_aisaev <aisaev-68@yandex.ru>
2023-10-07 15:49:18 +0300

    fix

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from catalog import models
 
 
@@ -26,14 +27,24 @@ class BookImageInline(admin.StackedInline):
 
 @admin.register(models.Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'isbn', 'publication_date', 'display_author', 'genre')
+    inlines = [BookImageInline]
+    list_display = ('id', 'cover_image_display', 'title', 'isbn', 'publication_date', 'display_author', 'genre')
     search_fields = ('title', 'isbn')
     filter_horizontal = ('authors',)
     list_filter = ('genre',)
     list_editable = ('publication_date',)  # Разрешаем редактирование в списке
-    inlines = [BookImageInline]
+
 
     def display_author(self, obj):
         return ', '.join([author.__str__() for author in obj.authors.all()])
 
     display_author.short_description = 'Авторы'
+
+    def cover_image_display(self, obj):
+        if obj.images.exists():
+            image_url = obj.images.first().image.url
+            return format_html('<a href="{}" target="_blank"><img src="{}" height="50" /></a>', image_url, image_url)
+        return "Изображение отсутствует"
+
+    cover_image_display.allow_tags = True
+    cover_image_display.short_description = 'Обложка'
